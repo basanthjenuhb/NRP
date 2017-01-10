9d9a3ed2f254cd94bd8e55a833b0ecc3a36de77b
Basanth Jenu H B <basanthjenuhb@gmail.com>
2017-01-10 09:26:40 +0530

    Some Changes

--- Neural Networks/Perceptron Training/XOR.py
+++ Neural Networks/Perceptron Training/XOR.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt
 import random
 
 def sigmoid(z, derivative=False):
@@ -17,16 +17,17 @@ def costFunction(x, y, W1, W2, b1, b2):
 
 x = [ [0,0],[0,1],[1,0],[1,1]]
 y = [ [a ^ b] for (a,b) in x ]
-
+# np.random.seed(10)
 x , y = np.array(x) , np.array(y)
 W1 , W2 = 2 * np.random.randn(4,2) - 1 , 2 * np.random.randn(1,4) - 1
 b1 , b2 = random.random() , random.random() 
-epochs , alpha = 100000 , 0.5
+epochs , alpha , costs = 100000 , .1 , []
 
 for i in range(epochs):
 	z1 , x1 , z2 , x2 , error , cost = costFunction(x, y, W1, W2, b1, b2)
-	if cost < 0.001:break
+	if cost < 0.01:break
 	print "Epoch",i,"Error:",cost
+	costs.append(cost)
 
 	delta2 = error * sigmoid(z2, derivative=True)
 	dw2 = np.dot(delta2.T, x1)
@@ -35,6 +36,13 @@ for i in range(epochs):
 	dw1 = np.dot(delta1.T,x)
 
 	W1, W2 = W1 - alpha * dw1, W2 - alpha * dw2
+	b1 , b2 = b1 - alpha * sum(sum(delta1)), b2 - alpha * sum(delta2)
+	# print b1,b2,"h"
 
+print "\nFinal Error:",cost
 print "Target:",y.T
 print "Output:",costFunction(x, y, W1, W2, b1, b2)[3].T
+plt.plot([j for j in range(len(costs))],costs)
+plt.xlabel('Iterations -->')
+plt.ylabel('Error -->')
+plt.show()
